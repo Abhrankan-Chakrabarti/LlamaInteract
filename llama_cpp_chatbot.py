b3c622b3af9b1e88929ce1a0bacbca007db00c2e
Abhrankan Chakrabarti <abhrankan@gmail.com>
2024-07-30 18:23:58 +0530

    Update llama_cpp_chatbot.py

--- llama_cpp_chatbot.py
+++ llama_cpp_chatbot.py
@@ -5,7 +5,7 @@ file = browse_for_file(extensions_list=('gguf',))
 import llama_cpp
 
 # Create a new Llama object
-llama_obj = llama_cpp.Llama(model_path=file, verbose=False)
+llama_obj = llama_cpp.Llama(model_path=file, verbose=False, n_ctx=2048)
 
 # REPL loop
 while True:
@@ -19,7 +19,7 @@ while True:
     try:
         # Evaluate the input using the llama_obj
         tokens = llama_obj.tokenize(user_input.encode())
-        for token in llama_obj.generate(tokens, top_k=40, top_p=0.95, temp=1.0, repeat_penalty=1.1):
+        for token in llama_obj.generate(tokens, top_k=40, top_p=0.95, temp=0.7, repeat_penalty=1.1):
             print(llama_obj.detokenize([token]).decode(), end="", flush=True)
         print()
     except BaseException as e:
