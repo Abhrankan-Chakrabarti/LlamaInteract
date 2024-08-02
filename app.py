40c00b6f5a9218bc1b85b0ab3c9caadda1182d73
Abhrankan-Chakrabarti <abhrankan@gmail.com>
2024-08-03 00:36:46 +0530

    Modify frontend and fix backend

--- app.py
+++ app.py
@@ -1,16 +1,59 @@
-from flask import *
+import json
+import threading
+import time
+from queue import Queue
+from flask import Flask, Response, render_template, request
+
 app = Flask(__name__)
 app.static_folder = 'static'
-@app.route("/")
-def home():
-    return render_template("index.html")
-@app.route("/get", methods=['GET', 'POST'])
+
+prompt_queue = Queue()
+sse_event_queue = Queue()
+response_thread = None
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    global prompt_queue
+
+    if request.method == 'POST':
+
+        prompt = request.form.get('prompt')
+
+        prompt_queue.put(prompt)
+
+        return {'status': 'success'}, 200
+
+    return render_template('index.html')
+
+def send_sse_data():
+    global prompt_queue, sse_event_queue, response_thread
+    while True:
+        if not prompt_queue.empty():
+            if response_thread and response_thread.is_alive():
+                continue
+
+            prompt = prompt_queue.get()
+
+            response_thread = threading.Thread(target=stream, args=(prompt, model))
+            response_thread.start()
+
+        while not sse_event_queue.empty():
+            sse_event = sse_event_queue.get()
+            yield f"data: {json.dumps(sse_event)}\n\n"
+
+        time.sleep(1)
+
+@app.route('/stream', methods=['GET'])
 def get_bot_response():
-    userText = request.args.get('msg') if request.method == "POST" else None
-    return Response(stream(input=userText, model=model) if request.method == "POST" else None, mimetype='text/event-stream')
+    def event_stream():
+        return send_sse_data()
+
+    return Response(event_stream(), mimetype='text/event-stream')
+
 def globalize(m, s):
     global model, stream
     model, stream = m, s
+
 def main(model, stream):
     globalize(model, stream)
-    app.run()
+    app.run(debug=True, use_reloader=False)
