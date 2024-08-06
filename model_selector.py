badbf0f3cc7b119ebd12eb05ce6b0cf15ad32935
Abhrankan-Chakrabarti <abhrankan@gmail.com>
2024-08-06 15:55:17 +0530

    Add file browsing for .gguf files and extract file name

--- model_selector.py
+++ model_selector.py
@@ -1,4 +1,8 @@
 from console_explorer import *
+import os
 
 file = browse_for_file(extensions_list=('gguf',))
-model = file.split(sep)[-1]
+if file:
+    model = os.path.basename(file)  # Safely extract the file name
+else:
+    model = None  # Handle case where no file was selected
